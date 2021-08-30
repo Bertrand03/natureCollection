f42cb3bac099382f63567b4e9babaf32d02adcc0
bertrand.cerot <bertrand.cerot@cgi.com>
2021-08-30 18:06:05 +0200

    affichage vue d'une plante. Recupération du recyclerView pour affichage en boucle

---
+++ app/src/main/java/fr/drako/naturecollection/adapter/PlantAdapter.kt
@@ -0,0 +1,39 @@
+package fr.drako.naturecollection.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import fr.drako.naturecollection.R
+
+// C'est cette classe qu'on va donner au RecyclerView pour pouvoir adapter pour chaque plante son équivalent en image
+// RecyclerView et notre classe maitresse, elle utilise la classe Adapter en lui préciseant le type de classe utilisée soit PlantAdapter.ViewHolder. ViewHolder est bien à l'intérieur de ma classe PlantAdapter
+class PlantAdapter : RecyclerView.Adapter<PlantAdapter.ViewHolder>(){
+
+    // Va permettre de porter la vue, de faire une sorte de boite à composants.
+    // Boite pour ranger tous les composants à controler
+    // On passe notre vue au ViewHolder mais on la passe également au composant maître Android "RecyclerView" qui va utiliser notre classe ViewHolder
+    class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        // image de la plante
+        val plantImage = view.findViewById<ImageView>(R.id.image_item)
+
+    }
+
+    // Méthode qui injecte (inflate) un composant
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =  LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_horizontal_plant, parent, false)
+        return ViewHolder(view)
+    }
+
+    // Mettre à jour chaque modele avec la plante en question
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    // Permet de renvoyer combien d'item on veut afficher dynamiquement
+    override fun getItemCount(): Int = 5
+
+}
--- app/src/main/java/fr/drako/naturecollection/fragments/HomeFragment.kt
+++ app/src/main/java/fr/drako/naturecollection/fragments/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import fr.drako.naturecollection.R
+import fr.drako.naturecollection.adapter.PlantAdapter
 
 /*On met un :Fragment() pour déclarer que c'est un fragment (va importer via l'import androidx*/
 class HomeFragment : Fragment() {
@@ -13,7 +15,13 @@ class HomeFragment : Fragment() {
     /*Cette fonction va se charger d'intégrer un layout via inflater*/
     /*Lors d'un appel via onCreateView on va charger le fragment déclaré dans le return dans notre HomeFragment*/
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater?.inflate(R.layout.fragment_home, container, false)
+        val view = inflater?.inflate(R.layout.fragment_home, container, false)
+
+        // Récupérer le recyclerView
+        val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
+        horizontalRecyclerView.adapter = PlantAdapter()
+
+        return view
     }
 
 }
