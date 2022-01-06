bed434a304c8175318f6685a37a3a5674340aa10
bertrand.cerot <bertrand.cerot@cgi.com>
2022-01-06 19:20:24 +0100

    commit intermediaire tuto à 2:26:10

--- app/src/main/java/fr/drako/naturecollection/MainActivity.kt
+++ app/src/main/java/fr/drako/naturecollection/MainActivity.kt
@@ -14,19 +14,18 @@ class MainActivity : AppCompatActivity() {
 
         // Mettre à jour la liste de plantes
         // Déclaration du callback d'instructions à faire après que les données aient été récupérées
+        // En rajoutant le callback dans mon repository je suis passé de repo.updateData () à repo.updateData {} avec l'appel à mon fragment
         repo.updateData {
-
+            // injecter le fragment dans notre boite (fragment_container)
+            // supportFragmentManager permet de gérer les fragments
+            val transaction = supportFragmentManager.beginTransaction()
+            // "R" doit représenter ma "root", mon projet global.
+            // Je pars donc de R pour trouver mon id, qui ici, s'appelle fragment_container (dans "activity_main.xml")
+            // Et je le remplace par le fragment qui se trouve dans HomeFragment avec l'appel de méthode "onCreateView())
+            transaction.replace(R.id.fragment_container, HomeFragment(this)) // Lorsqu'on va créer le homefragment on va lui donner l'activité principale en paramètre (va voir dans HomeFragment pour la suite de l'explication)
+            // Permet de ne pas avoir de retour sur ce composant.
+            transaction.addToBackStack(null)
+            transaction.commit()
         }
-
-        // injecter le fragment dans notre boite (fragment_container)
-        // supportFragmentManager permet de gérer les fragments
-        val transaction = supportFragmentManager.beginTransaction()
-        // "R" doit représenter ma "root", mon projet global.
-        // Je pars donc de R pour trouver mon id, qui ici, s'appelle fragment_container (dans "activity_main.xml")
-        // Et je le remplace par le fragment qui se trouve dans HomeFragment avec l'appel de méthode "onCreateView())
-        transaction.replace(R.id.fragment_container, HomeFragment(this)) // Lorsqu'on va créer le homefragment on va lui donner l'activité principale en paramètre (va voir dans HomeFragment pour la suite de l'explication)
-        // Permet de ne pas avoir de retour sur ce composant.
-        transaction.addToBackStack(null)
-        transaction.commit()
     }
 }
--- app/src/main/java/fr/drako/naturecollection/PlantModel.kt
+++ app/src/main/java/fr/drako/naturecollection/PlantModel.kt
@@ -2,6 +2,7 @@ package fr.drako.naturecollection
 
 //Après PlantModel on supprime les accolades et on met des parenthèses pour lister et donner toutes les caractéristiques d'une plante
 //J'ai créé cette classe au moment où, dans le cours, on cherche à remplacer l'image par défaut répétée à chaque fois. On veut afficher des plantes différentes dans notre liste
+// Après avoir créé ma bdd et déclarer mes plantes, quand j'ajoute une plante sur Firebase je n'ai pas besoin de relancer mon application pour que ça rafraichisse les valeurs. Ca le fait tout seul.
 
 class PlantModel (
     // val pour valeur que l'on attribue. valeur fixe ?
